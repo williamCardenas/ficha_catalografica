f5a642ea0179839d1363a62aa183b1004fbdabf2
William Caetano Cardenas <william.caetano.cardenas@gmail.com>
2018-02-27 17:11:53 -0300

    instalação dos componentes do synfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+
+    /**
+    * @Route("/")
+    */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
